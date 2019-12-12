acf4ff409e8e1c3d0a9f69a495958d469461a883
bluescipii <bluiescipii@gmail.com>
2019-12-12 15:35:11 +0900

    Updates

---
+++ precache-manifest.cdb3f9c8e425f558d75080055811b109.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a88ae1c550586d41372b2528aa8f3d1",
+    "url": "/bluescipii/physical-workshop.git/index.html"
+  },
+  {
+    "revision": "cfa907fd3c1816754a60",
+    "url": "/bluescipii/physical-workshop.git/static/css/2.7fd08c2a.chunk.css"
+  },
+  {
+    "revision": "73260f404e3f888fdbb7",
+    "url": "/bluescipii/physical-workshop.git/static/css/main.c7ebab60.chunk.css"
+  },
+  {
+    "revision": "cfa907fd3c1816754a60",
+    "url": "/bluescipii/physical-workshop.git/static/js/2.6d13b545.chunk.js"
+  },
+  {
+    "revision": "73260f404e3f888fdbb7",
+    "url": "/bluescipii/physical-workshop.git/static/js/main.7942bfe5.chunk.js"
+  },
+  {
+    "revision": "06d4dafd70a1dc19bd00",
+    "url": "/bluescipii/physical-workshop.git/static/js/runtime-main.2bf5b96c.js"
+  },
+  {
+    "revision": "b8005a06febdca1f23525071b14e43f7",
+    "url": "/bluescipii/physical-workshop.git/static/media/Apl2741.b8005a06.ttf"
+  },
+  {
+    "revision": "8c7880d87d40918ec9c70e9de12a219d",
+    "url": "/bluescipii/physical-workshop.git/static/media/CodeNewRoman-aOAK.8c7880d8.ttf"
+  },
+  {
+    "revision": "988b2589dbb0531ff82a7f539b77cd70",
+    "url": "/bluescipii/physical-workshop.git/static/media/D2Coding-Ver1.3.2-20180524.988b2589.ttf"
+  },
+  {
+    "revision": "c2356fc49835b1870dcc5b07799b4920",
+    "url": "/bluescipii/physical-workshop.git/static/media/DejaVuSansMono.c2356fc4.ttf"
+  },
+  {
+    "revision": "d9ab192896af1ab48c760f3368842b79",
+    "url": "/bluescipii/physical-workshop.git/static/media/Hack-Regular.d9ab1928.ttf"
+  },
+  {
+    "revision": "9298077dbed902cc127c5978d2ebffc1",
+    "url": "/bluescipii/physical-workshop.git/static/media/Hermit-Regular.9298077d.otf"
+  },
+  {
+    "revision": "f1158af6bd26072b60ae1fc1af0a11ba",
+    "url": "/bluescipii/physical-workshop.git/static/media/Inconsolata.f1158af6.ttf"
+  },
+  {
+    "revision": "c353e3dabba482f3721f67c6a43f1744",
+    "url": "/bluescipii/physical-workshop.git/static/media/MesloLGM-Regular.c353e3da.ttf"
+  },
+  {
+    "revision": "9b27db986ad2a74c522e9d3b6f7e2a63",
+    "url": "/bluescipii/physical-workshop.git/static/media/Monoid-Regular.9b27db98.ttf"
+  },
+  {
+    "revision": "97dd0d3d0eaf3e254fe31cdc0b091f2d",
+    "url": "/bluescipii/physical-workshop.git/static/media/NotCourierSans.97dd0d3d.ttf"
+  },
+  {
+    "revision": "af3bd30eb752a50f47811c699aded99f",
+    "url": "/bluescipii/physical-workshop.git/static/media/Nouveau_IBM.af3bd30e.ttf"
+  },
+  {
+    "revision": "51458c49387e34108ead6b220f699d54",
+    "url": "/bluescipii/physical-workshop.git/static/media/Perfect-DOS-VGA-437.51458c49.ttf"
+  },
+  {
+    "revision": "361ffa4ca82d2a1841d178464353e955",
+    "url": "/bluescipii/physical-workshop.git/static/media/ProggyClean.361ffa4c.ttf"
+  },
+  {
+    "revision": "472c7e7a808efce9161fb8f27535bf22",
+    "url": "/bluescipii/physical-workshop.git/static/media/SourceCodePro-Medium.472c7e7a.ttf"
+  },
+  {
+    "revision": "338ca871541321cdab63499c53687c05",
+    "url": "/bluescipii/physical-workshop.git/static/media/VictorMono-Regular.338ca871.ttf"
+  },
+  {
+    "revision": "fe4a6c771135c7d60c684b564466e82a",
+    "url": "/bluescipii/physical-workshop.git/static/media/consola.fe4a6c77.ttf"
+  },
+  {
+    "revision": "fe843eb80bee993eaa9f3baf897949d8",
+    "url": "/bluescipii/physical-workshop.git/static/media/iosevka-medium.fe843eb8.ttf"
+  },
+  {
+    "revision": "446967587adbf3a0e49e0b3745e345f7",
+    "url": "/bluescipii/physical-workshop.git/static/media/mplus-1m-medium.44696758.ttf"
+  }
+]);
